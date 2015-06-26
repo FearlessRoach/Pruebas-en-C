5e4a51102f219067ccbdd9913a4da0e6ff34c67c
cronosdead <gaia.cane1@gmail.com>
2015-06-26 15:26:36 -0500

    Agregando carpeta del Capítulo 2

---
+++ Capitulo-2/exercise-2-1.c
@@ -0,0 +1,15 @@
+/* Prints and calculates the sizes of data types
+   embedded in C lenguage.
+*/
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+	int x = 0;
+  	int i = 0;
+	
+	i = sizeof x;
+	printf("%.10e", FLT_MAX);
+  	return 0;
+}
