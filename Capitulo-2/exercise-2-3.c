8e910df6b00d2616519e236477a955bf1fb4cedc
cronosdead <gaia.cane1@gmail.com>
2015-07-01 12:51:32 -0500

    Ejercicio 2.3 terminado

--- Capitulo-2/exercise-2-3.c
+++ Capitulo-2/exercise-2-3.c
@@ -10,9 +10,12 @@ int slen(char string[]);
 
 int main()
 {
-	char s[4] = "F45D";
-
+	char s[20] = "3da5Fg\0";
+	int n;
 	
+	n = htoi(s);
+	printf("%d", n);
+	return 0;
 }
 
 int htoi(char s[])
@@ -23,46 +26,41 @@ int htoi(char s[])
 	len = slen(s);
 	if(s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
 	{
-		for(i = 2; i < len; i++)
+		for(i = len-1; i > 1; i--)
 		{
 			if(s[i] >= '0' && s[i] <= '9')
-			{
-				res = [i] - '0';
-			}
+				res = res + (s[i] - '0')*pow(16, len-1-i);
 			else if(s[i] >= 'a' && s[i] <= 'f')
-			{
-				res = s[i] - 'a' + 10;
-			}
+				res = res + (s[i] - 'a' + 10)*pow(16, len-1-i);
 			else if(s[i] >= 'A' && s[i] <= 'F')
-			{
-				res = s[i] - 'A' + 10;
-			}
+				res = res + (s[i] - 'A' + 10)*pow(16, len-1-i);
 			else
 			{
-				printf("The \'%c\' value does not correspond \ 
-						  with HEX symbol.\n", s[i]);
-				exit(0);
-			}
-
-	}
-	else
-	{
-		for(i = 0; i < len; i++)
-		{
-			if(s[i] >= '0' && s[i] <= '9')
-				res = res + s[i] - '0';
-			else if(s[i] >= 'a' && s[i] <= 'f')
-				res = res + s[i] - 'a' + 10;
-			else if(s[i] >= 'A' && s[i] <= 'F')
-				res = res + s[i] - 'A' + 10;
-			else
-			{
-				printf("The \'%c\' value does not correspond \ 
-						  with HEX symbol.\n", s[i]);
+				printf("The \'%c\' value does not correspond " 
+						 "with HEX symbol.\n", s[i]);
 				exit(0);
 			}
 		}
 	}
+	else
+	{
+		for(i = len-1; i >= 0; i--)
+		{
+			if(s[i] >= '0' && s[i] <= '9')
+				res = res + (s[i] - '0')*pow(16, len-1-i);
+			else if(s[i] >= 'a' && s[i] <= 'f')
+				res = res + (s[i] - 'a' + 10)*pow(16, len-1-i);
+			else if(s[i] >= 'A' && s[i] <= 'F')
+				res = res + (s[i] - 'A' + 10)*pow(16, len-1-i);
+			else
+			{
+				printf("The \'%c\' value does not correspond " 
+						 "with HEX symbol.\n", s[i]);
+				exit(0);
+			}
+		}
+	}
+	return res;
 }
 
 int slen(char s[])
