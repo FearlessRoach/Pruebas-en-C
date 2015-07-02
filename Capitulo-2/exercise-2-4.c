5b5d14e2f36eb3609a82703a463fccc50dbf1f7b
cronosdead <gaia.cane1@gmail.com>
2015-07-02 09:35:46 -0500

    Terminando ejercicio 2.4

--- Capitulo-2/exercise-2-4.c
+++ Capitulo-2/exercise-2-4.c
@@ -7,16 +7,30 @@ void squeeze2(char s1[], char s2[]);
 
 int main()
 {
-		
+	char s1[30] = "prueba ghj rtm miro squeeze";
+	char s2[30] = "prueba de squeeze";
+	
+	printf("%s\n", s1);
+	squeeze2(s1, s2);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	return 0;
 }
 
-void squeeze2(char s1[], char s2[])
+void squeeze2(char s1[], char s2[]) 
 {
+	/* Function to 'squeeze' a string (s1) 
+	 * against other string (s2). 
+	 */
 	int i, j, k;
 
-	for(i = k = 0; s1[i] != '\0'; i++)
-		for(j = 0; s2[j] != '\0'; j++)
-			if(s[j] != s[i])
-				s[k++] = s[i];
-	s[k] = '\0';
+	for(i = 0; s2[i] != '\0'; i++) 
+	{
+		/* Comparing a character from 's2' with entire 's1' string */
+		for(j = k = 0; s1[j] != '\0'; j++)
+			if(s2[i] != s1[j] || s1[j] == ' ')
+				/* If the char's are different, add 's1' char to its begining */
+				s1[k++] = s1[j];
+	}
+	s1[k] = '\0';
 }
