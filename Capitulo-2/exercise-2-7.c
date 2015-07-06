eed6ee1372e8055f32a239737a2ff13df13ce1c5
cronosdead <gaia.cane1@gmail.com>
2015-07-06 10:32:37 -0500

    Ejercicio 2.7 terminado

--- Capitulo-2/exercise-2-7.c
+++ Capitulo-2/exercise-2-7.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-
 unsigned inverted(unsigned, int, int);
 unsigned getbits(unsigned, int, int);
 
 int main()
 {
-	printf("%d", 3 >> 2);
+	unsigned bit;
+
+	printf("%d", inverted(8, 1, 2));
 	return 0;
 }
 
 unsigned inverted(unsigned x, int p, int n)
 {
+	/* Inverts 'n' bits from position 'p' of 'x' */
 	unsigned b;
 	
-	b = ~getbits(x, p, n);
-	b = (b >> (x-p+1)) << (p+1-n);
-	return; 
+	b = getbits(x, p, n); /* Gets the bits to invert */
+	b = (b | ~(~0 << n)) << (p+1-n); 
+	return b ^ x; /* Applies XOR to contrast 'b' into 'x' */
 }
 
 unsigned getbits(unsigned x, int p, int n)
