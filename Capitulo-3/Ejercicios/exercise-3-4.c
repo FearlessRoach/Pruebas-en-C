e78a03d9d6ca8ddfaff998f59d0fa3cb25025353
cronosdead <gaia.cane1@gmail.com>
2015-07-09 17:38:00 -0500

    Ejercicio 3.4 terminado y ejercicio 3.5 en proceso

---
+++ Capitulo-3/Ejercicios/exercise-3-4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+void reverse(char s[]);
+void itoa(unsigned int n, char s[]);
+
+int main()
+{
+	char s[100] = {'\0'};
+	int c;
+	c = INT_MIN;
+	printf("Como tipo entero\t\t--> %d\n", c);
+	itoa(c, s);
+	printf("Como tipo cadena de caracteres  --> %s", s);	
+	return 0;
+}
+
+/* itoa: convert 'num' to characters in 's' */
+void itoa(unsigned int num, char s[])
+{
+	int i, sign;
+	
+	if((sign = num) < 0) /* Record sign */ 
+		num = -num;	/* Make 'num' positive */
+	i = 0;
+	do	/* Generate digits in reverse order */
+	{
+		s[i++] = num % 10 + '0';	/* Get next digit */
+	} while ((num /= 10) > 0);	/* Delete it */
+	if(sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
+
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for(i = 0, j = strlen(s)-1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
---
+++ Capitulo-3/Ejercicios/exercise-3-5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+void itob(int n, char s[], unsigned short b);
+
+int main()
+{
+	
+}
+
+void itob(int n, char s[], unsigned short b)
+{
+	int i, token, res;
+
+	s = {'\0'};
+	switch(b)
+	{
+		base = 0;
+		token = n/b;
+		s[0] = (n % b) >= 10 ? '0' : 'A';
+		for(i = token-1; i > 0; i--)
+			s[i] = strcat(pow(b, i))
+	}
+}
